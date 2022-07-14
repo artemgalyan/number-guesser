3b7cf3994743d38a0eba2a75a66668efe159d97e
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2022-07-14 14:32:47 +0300

    small fixes

--- app/src/main/java/com/example/numberguesser/game_fragment.kt
+++ app/src/main/java/com/example/numberguesser/game_fragment.kt
@@ -10,21 +10,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import kotlin.random.Random
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [game_fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class game_fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     private var guesses: Int = 0
         set(value) {
             field = value
@@ -38,10 +29,7 @@ class game_fragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        arguments?.let { }
     }
 
     override fun onCreateView(
@@ -116,19 +104,12 @@ class game_fragment : Fragment() {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment game_fragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             game_fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+                arguments = Bundle().apply {}
             }
     }
 }
